a7b58d33a8117c9fee2144f5a261007787efc3c9
João Almeida <toxicyszconta@gmail.com>
2024-11-18 03:57:44 -0800

    11/18 iacitel oma et

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-// function progresso() {
-//     const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-//     const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-//     const scrollPercentage = (scrollTop / scrollHeight) * 100;
+function progresso() {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrollPercentage = (scrollTop / scrollHeight) * 100;
 
-//     document.querySelector(".indicador").style.width = scrollPercentage + "%";
-// }
+    document.querySelector(".indicador").style.width = scrollPercentage + "%";
+}
 
-// function progressoAtivo() {
-//     const testElement = document.createElement("div");
-//     testElement.style.animationTimeline = "scroll()";
-//     return testElement.style.animationTimeline !== "";
-// }
-
-// if (!progressoAtivo()) {
-//     console.log("animation-timeline não é suportado. Ativando Javascript.");
-//     window.addEventListener("scroll", progresso);
-// } else {
-//     console.log("animation-timeline é suportado.");
-// }
+window.addEventListener("scroll", progresso);
